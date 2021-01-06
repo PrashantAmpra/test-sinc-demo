cc603aef39445975dd6b9e56d49cbefc23ade13f
Prashant <prashant.m@ampra.in>
2021-01-06 18:36:19 +0530

    first commit

---
+++ src/sys_script_include/TestOne/script.js
@@ -0,0 +1,7 @@
+var TestOne = Class.create();
+TestOne.prototype = {
+  // Test 123
+  initialize: function() {},
+
+  type: "TestOne",
+};
---
+++ src/sys_script_include/TestTwo/script.js
@@ -0,0 +1,7 @@
+var TestTwo = Class.create();
+TestTwo.prototype = {
+    initialize: function() {
+    },
+
+    type: 'TestTwo'
+};
